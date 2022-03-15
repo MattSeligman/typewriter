8d448a4f27116df694d298fddd043ca05d12ab58
MattSeligman <matt@modelitup.com>
2022-03-15 16:36:04 -0700

    revised 'thesymbol' temporary variable used in the for loop to 'symbol'

--- spinner2.js
+++ spinner2.js
@@ -2,9 +2,9 @@ process.stdout.write('hello from spinner1.js... \rheyyy\n');
 const message = ` <-- Woa!, check out the amazing spinner!`; //optional fun lol (replace with "" or remove ${message} from line 2 if removed)
 const symbols = [`|${message}`,`/`,`-`,`\\`,`|`,`/`,`-`,`\\`,`|`,`/`,`-`,`\\`,`|`,`\n`];
 let delaySpeed = 0;
-const loopIt = (thesymbol, delaySpeed) => {
+const loopIt = (symbol, delaySpeed) => {
   setTimeout(() => {
-    process.stdout.write(`\r${thesymbol}`);
+    process.stdout.write(`\r${symbol}`);
   }, delaySpeed);
 };
 symbols.forEach((symbol)=>{
