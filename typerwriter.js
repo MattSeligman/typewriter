a43e4a81f6bf3c14ac3a93dff29acf0debcca1ad
MattSeligman <matt@modelitup.com>
2022-03-15 09:47:49 -0700

    Setup framework for a Typerwriter using a timer, forLoop, process.stdout.write and a setTimeout function

---
+++ typerwriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+let timer = 0;
+
+for (const char of sentence) {
+
+    setTimeout(() => {
+        process.stdout.write(char);
+      }, timer);
+
+      timer = timer + 50;
+}
+
+setTimeout(() => {
+    process.stdout.write("\n");
+  }, timer);
