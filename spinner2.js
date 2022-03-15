a8ad82f9788423bc5f32f448257af490c3bf2063
MattSeligman <matt@modelitup.com>
2022-03-15 16:27:54 -0700

    Refined the spinner into a loopable function with some additional fun parameters.

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+const message = ` <-- Woa!, check out the amazing spinner!`; //optional fun lol (replace with "" or remove ${message} from line 2 if removed)
+const symbols = [`|${message}`,`/`,`-`,`\\`,`|`,`/`,`-`,`\\`,`|`,`/`,`-`,`\\`,`|`,`\n`];
+let delaySpeed = 0;
+const loopIt = (thesymbol, delaySpeed) => {
+  setTimeout(() => {
+    process.stdout.write(`\r${thesymbol}`);
+  }, delaySpeed);
+};
+symbols.forEach((symbol)=>{
+  delaySpeed += 300;
+  loopIt(symbol, delaySpeed);
+});
